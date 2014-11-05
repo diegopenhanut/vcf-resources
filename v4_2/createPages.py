ebe94e233dc8547d1f14a4a1361f4f20d48e502d
diegopenhanut <diegopenhanut@gmail.com>
2014-11-05 13:45:01 -0600

    main page for v4_2

--- v4_2/createPages.py
+++ v4_2/createPages.py
@@ -4,14 +4,25 @@ import os
 def generateHtml (directory ,title, content):
     fh = open(directory+"/index.html", "w")
     lines_of_text = ["<html>\n<head>\n<title>",
-            title,"</title>\n</head>\n",
-            content,"\n",
+            title,"</title>\n</head>\n<body>",
+            content,"\n</body>",
             "</html>"]
     fh.writelines(lines_of_text)
     fh.close()
     return
 
-with open('structure.csv', 'rb') as f:
+with open('structure.csv', 'rU') as f:
+    reader = csv.reader(f)
+    fh = open('index.html', 'w')
+    fh.writelines("<html>\n<head></head>\n<body>\n")
+    for row in reader:
+        fh.writelines("<a href='"+row[0]+"'>"+row[0]+"</a><br>\n")
+    fh.writelines("</body>\n</html>")
+    fh.close()
+
+
+
+with open('structure.csv', 'rU') as f:
     reader = csv.reader(f)
     for row in reader:
         os.mkdir(row[0])
