7d63509c55d458cf215b27dcdb6c8f88ca16ef79
diegopenhanut <diegopenhanut@gmail.com>
2016-03-14 07:32:53 -0500

    add bootstrap

--- v4_2/createPages.py
+++ v4_2/createPages.py
@@ -1,25 +1,49 @@
-#!/user/bin/python
+#!/usr/bin/env python
 import csv
 import os
 
 def generateHtml (directory ,title, content):
     fh = open(directory+"/index.html", "w")
-    lines_of_text = ["<html>\n<head>\n<title>",
-            title,"</title>\n</head>\n<body>",
-            content,"\n</body>",
-            "</html>"]
+    with open('../template.html', 'r') as template_file:
+        top = template_file.read()
+    
+    with open('../template_mid.html', 'r') as template_mid_file:
+        mid = template_mid_file.read()
+    
+    with open('../template_bottom.html', 'r') as template_bottom_file:
+        bottom = template_bottom_file.read()
+
+    lines_of_text = [top,
+            title,
+            mid,
+            "<h1>", title, "</h1>",
+            content,
+            bottom]
+    
     fh.writelines(lines_of_text)
     fh.close()
     return
+
 # older version don't work with "with"
 # fix it later
 with open('structure.csv', 'rU') as f:
     reader = csv.reader(f)
     fh = open('index.html', 'w')
-    fh.writelines("<html>\n<head></head>\n<body>\n")
+    with open('../template.html', 'r') as template_file:
+        top = template_file.read()
+    
+    with open('../template_mid.html', 'r') as template_mid_file:
+        mid = template_mid_file.read()
+    
+    with open('../template_bottom.html', 'r') as template_bottom_file:
+        bottom = template_bottom_file.read()
+    title = "Main Class's"
+    fh.writelines([top, title , mid, "<h1>", title, "</h1>"])
+    
     for row in reader:
         fh.writelines("<a href='"+row[0]+"'>"+row[0]+"</a><br>\n")
-    fh.writelines("</body>\n</html>")
+    
+    fh.writelines(bottom)
     fh.close()
 
 
